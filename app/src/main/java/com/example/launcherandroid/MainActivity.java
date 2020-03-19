c95c6acc9a9070a3c00a5ea9f1e223bf8331b19b
pokluk123 <ales.pokluk@gmail.com>
2020-03-19 10:38:29 +0100

    buttons

--- app/src/main/java/com/example/launcherandroid/MainActivity.java
+++ app/src/main/java/com/example/launcherandroid/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        ImageView chrome = findViewById(R.id.chromeButton);
+        chrome.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
+                startActivity(launchIntent);
+            }
+        });
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
+        ImageView gmail = findViewById(R.id.gmailButton);
+        gmail.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.gmail");
+                startActivity(launchIntent);
+            }
+        });
+
+        ImageView calculator = findViewById(R.id.calculatorButton);
+        calculator.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.calculator");
+                startActivity(launchIntent);
+            }
+        });
+
     }
 }

4a5b701f6bd591d633ff98f964c0dd2e5d82e48d
pokluk123 <ales.pokluk@gmail.com>
2020-03-27 20:58:26 +0100

    button as

--- app/src/main/java/com/example/launcherandroid/MainActivity.java
+++ app/src/main/java/com/example/launcherandroid/MainActivity.java
@@ -11,9 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,33 +47,7 @@ public class MainActivity extends AppCompatActivity {
             novo.setImageDrawable(ikonica);
             ll.addView(novo);
         }
-
-
-
-
-        /*final PackageManager pm = getPackageManager();
-        //get a list of installed apps.
-        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-        Log.e("ales", "stevilo packagov: "+ packages.size());
-        for (ApplicationInfo packageInfo : packages) {
-            Log.e("ales", "Installed package :" + packageInfo.packageName);
-            Log.e("ales", "Source dir : " + packageInfo.sourceDir);
-            Log.e("ales", "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
-        }*/
-        // the getLaunchIntentForPackage returns an intent that you can use with startActivity()
-
-/*
-        ImageView chromeIcon = findViewById(R.id.chromeImageView);
-        chromeIcon.setOnClickListener(onChromeButtonClick);
-        chromeIcon.setImageDrawable(getActivityIcon(this,"com.android.chrome", "com.google.android.apps.chrome.Main"));
-
-        ImageView contacts = (ImageView) findViewById(R.id.contactsImageView);
-        contacts.setOnClickListener(onChromeButtonClick);
-        contacts.setImageDrawable(getActivityIcon(this,"com.android.contacts", "com.android.contacts.apps.contacts.Main"));
-        */
-
     }
-
     private View.OnClickListener onChromeButtonClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -90,14 +62,8 @@ public class MainActivity extends AppCompatActivity {
             startActivity(launchIntent);
         }
     };
-
-
     public static Drawable getActivityIcon(Context context, String packageName, String activityName) {
-        /*PackageManager pm = context.getPackageManager();
-        Intent intent = new Intent();
-        intent.setComponent(new ComponentName(packageName, activityName));
-        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
-        return resolveInfo.loadIcon(pm);*/
+
         try{
             Drawable icon = context.getPackageManager().getApplicationIcon(packageName);
             return icon;
@@ -107,123 +73,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
 }
 
 
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        ImageView chrome = (ImageView) findViewById(R.id.chromeImageView);
-        chrome.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                chrome.setImageDrawable(getActivityIcon(this,"com.android.chrome", "com.google.android.apps.chrome.Main"));
-            }
-        }
-    }
-        private View.OnClickListener onChromeButtonClick = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
-                startActivity(launchIntent);
-            }
-        };
-        /*
-        ImageView photo = (ImageView) findViewById(R.id.photoImageView);
-        ImageView dialer = (ImageView) findViewById(R.id.dialerImageView);
-        ImageView contacts = (ImageView) findViewById(R.id.contactsImageView);
-        photo.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.google.android.apps.photos");
-                startActivity(launchIntent);
-            }
-        });
-
-        dialer.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.dialer");
-                startActivity(launchIntent);
-            }
-        });
-
-        contacts.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.contacts");
-                startActivity(launchIntent);
-            }
-        });
-
-
-
-       /* ScrollView vsiKnofi = findViewById(R.id.neki);
-
-       for(ResolveInfo res: pkgAppsList){
-            Log.e("ales", res.toString());
-            ImageView enKnof = new ImageView(getApplicationContext());
-
-           vsiKnofi.addView(enKnof);
-       }
-
-        /*ImageView chromeIcon = (ImageView) findViewById(R.id.chromeButton);
-        chromeIcon.setImageDrawable(this, getActivityIcon(,"com.android.chrome", "com.google.android.apps.chrome.Main"));
-        */
-
-
-        /*ImageView chrome = findViewById(R.id.chromeButton);
-        chrome.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
-                startActivity(launchIntent);
-            }
-        });
-
-        ImageView phone = findViewById(R.id.phoneButton);
-        phone.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.phone");
-                startActivity(launchIntent);
-            }
-        });
-        //calculator ni naložen na samem sistemu
-        ImageView calculator = findViewById(R.id.calculatorButton);
-        calculator.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.calculator");
-                startActivity(launchIntent);
-            }
-        });
-
-        ImageView gmail = findViewById(R.id.gmailButton);
-        gmail.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
-                startActivity(launchIntent);
-            }
-        });
-        Button button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent launchIntent = new Intent(MainActivity.this,AppsDrawer.class);
-                startActivity(launchIntent);
-            }
-        });
-
-        public static Drawable getActivityIcon(View.OnClickListener context, String packageName, String activityName) {
-        PackageManager pm = context.getPackageManager();
-        Intent intent = new Intent();
-        intent.setComponent(new ComponentName(packageName, activityName));
-        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
-
-        return resolveInfo.loadIcon(pm);
-}
-
-
-*/
 
 
 

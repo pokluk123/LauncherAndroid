7e83d805769f9a3146011437254c6e98c5aa8f7f
znidi znidi <znidi26@gmail.com>
2020-07-23 13:00:26 +0200

    try this shit

--- app/src/main/java/com/example/launcherandroid/MainActivity.java
+++ app/src/main/java/com/example/launcherandroid/MainActivity.java
@@ -13,53 +13,61 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
+import org.bson.Document;
 import org.json.JSONObject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 import java.util.List;
 
 
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String serverIp="";
+    private static String serverPort="";
+    private static OkHttpClient client = new OkHttpClient();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue= Volley.newRequestQueue(this);
         String url="rask.cening.si/android.ip";
-        String ip="";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        ip=
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-
-                    }
-                });
-
-// Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
 
 
-        );
+
+        Request request = new Request.Builder()
+                .url("https://www.rask.cening.si/android.ip")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e("znidi", "error on receive IP " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    Log.e("znidi", "error on receive IP weird code " + response.code());
+                } else {
+                    String body = response.body().toString();//dobimo jedro sporočila
+                    Document doc = Document.parse(body);//"parsamo" v JSON
+                    Document mobilos = doc.get("mobilos", Document.class); //vzamemo ven mobilos JSON
+                    serverIp = mobilos.getString("ip");
+                    serverPort = mobilos.getString("port");
+                    Log.i("znidi", "prejeli smo server ip in port: "+serverIp + ":"+serverPort);
+                }
+            }
+        });
 
         Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -67,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout ll = findViewById(R.id.vseIkonce);
         for(ResolveInfo info:pkgAppsList){
-            RequestQueue
+            //RequestQueue
 
             String packageName = info.resolvePackageName;
             Drawable ikonica = null;
